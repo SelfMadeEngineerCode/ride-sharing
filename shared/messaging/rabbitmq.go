de07f4f6c5973d182833975b070df8144625805a
Tiago Taquelim <tiago.taquelim.ferreira@gmail.com>
2025-06-12 10:33:41 +0100

    feat: trip accept flow

--- shared/messaging/rabbitmq.go
+++ shared/messaging/rabbitmq.go
@@ -168,7 +168,14 @@ func (r *RabbitMQ) setupExchangesAndQueues() error {
 	); err != nil {
 		return err
 	}
-	
+
+	if err := r.declareAndBindQueue(
+		NotifyDriverAssignQueue,
+		[]string{contracts.TripEventDriverAssigned},
+		TripExchange,
+	); err != nil {
+		return err
+	}
 
 	return nil
 }
